9739b2d0b372108d232ffe42e414731fe4b8299a
Aditya K Goyal <101977885+Aditya-K-Goyal@users.noreply.github.com>
2022-07-21 12:40:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
+objects = [];
 img="";
+status="";
+
 function setup()
 {
-    canvas = createCanvas(640 ,420);
+    canvas = createCanvas(380 ,380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status :Detecting Objects";
+}
+function modelLoaded()
+{
+console.log("Model Loaded");
+status = true;
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 function preload()
 {
-    img = loadImage("dog_cat.jpg");
 }
 function draw()
 {
-    image(img,0,0,640,420);
-    fill("#FF0000");
-    text("dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);
+    image(video,0,0,380,380);
+    if(status !=""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video,gotResults);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Detecting Objects ";
+            document.getElementById("number_of_objects").innerHTML ="Number of objects detected are : "+ objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label +  " " + percent + "%" + objects[i].x + objects[i].y );
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
